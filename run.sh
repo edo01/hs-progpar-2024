8aaa1131beb4fe8004e81c9ba918e89ebd0a016a
edo01 <edocarra012@gmail.com>
2024-09-20 17:26:39 +0200

    Fix a bug

--- run.sh
+++ run.sh
@@ -27,13 +27,13 @@ then
 fi
 # make an array to store the execution time of each kernel
 declare -a D2
-declare -a CA52
+declare -a CA57
 
 #assign big values to the arrays
 for i in {1..5}
 do
     D2[$i]=100000000
-    CA52[$i]=100000000
+    CA57[$i]=100000000
 done
 
 for KERNEL in "default" "default_nb" "optim1" "optim2" "optim3" "optim4" 
@@ -42,7 +42,7 @@ do
     for i in {0..3}
     do
         D2[$i]=100000000
-        CA52[$i]=100000000
+        CA57[$i]=100000000
     done
 
     for i in {1..5} 
@@ -50,10 +50,10 @@ do
         echo "Running kernel $KERNEL" >&2
         # O0
         D2_0=$(taskset -c 1 ./bin0/easypap -k blur --no-display --load-image images/1024.png -i 100 -v seq -wt $KERNEL 2>&1| tail -1)
-        CA52_0=$(taskset -c 0 ./bin0/easypap -k blur --no-display --load-image images/1024.png -i 100 -v seq -wt $KERNEL 2>&1| tail -1)
+        CA57_0=$(taskset -c 0 ./bin0/easypap -k blur --no-display --load-image images/1024.png -i 100 -v seq -wt $KERNEL 2>&1| tail -1)
 
         echo "D2_0: $D2_0 ms" >&2
-        echo "CA52_0: $CA52_0 ms" >&2
+        echo "CA57_0: $CA57_0 ms" >&2
 
         ## if the execution time is less than the previous one, we update the value in the array
         if [ $(echo "$D2_0 < ${D2[0]}" | bc) -eq 1 ]
@@ -62,17 +62,17 @@ do
             echo "update D2_0" >&2
         fi
 
-        if [ $(echo "$CA52_0 < ${CA52[0]}" | bc) -eq 1 ]
+        if [ $(echo "$CA57_0 < ${CA57[0]}" | bc) -eq 1 ]
         then
-            CA52[0]=$CA52_0
-            echo "update CA52_0" >&2
+            CA57[0]=$CA57_0
+            echo "update CA57_0" >&2
         fi
 
         # O1
         D2_1=$(taskset -c 1 ./bin1/easypap -k blur --no-display --load-image images/1024.png -i 100 -v seq -wt $KERNEL 2>&1| tail -1)
         echo "D2_1: $D2_1 ms" >&2
-        CA52_1=$(taskset -c 0 ./bin1/easypap -k blur --no-display --load-image images/1024.png -i 100 -v seq -wt $KERNEL 2>&1| tail -1)
-        echo "CA52_1: $CA52_1 ms" >&2
+        CA57_1=$(taskset -c 0 ./bin1/easypap -k blur --no-display --load-image images/1024.png -i 100 -v seq -wt $KERNEL 2>&1| tail -1)
+        echo "CA57_1: $CA57_1 ms" >&2
 
         ## if the execution time is less than the previous one, we update the value in the array
         if [ $(echo "$D2_1 < ${D2[1]}" | bc) -eq 1 ]
@@ -81,17 +81,17 @@ do
             echo "update D2_1" >&2
         fi
 
-        if [ $(echo "$CA52_1 < ${CA52[1]}" | bc) -eq 1 ]
+        if [ $(echo "$CA57_1 < ${CA57[1]}" | bc) -eq 1 ]
         then
-            CA52[1]=$CA52_1
-            echo "update CA52_1" >&2
+            CA57[1]=$CA57_1
+            echo "update CA57_1" >&2
         fi
 
         # O2
         D2_2=$(taskset -c 1 ./bin2/easypap -k blur --no-display --load-image images/1024.png -i 100 -v seq -wt $KERNEL 2>&1| tail -1)
-        echo "CA52_2: $CA52_2 ms" >&2
-        CA52_2=$(taskset -c 0 ./bin2/easypap -k blur --no-display --load-image images/1024.png -i 100 -v seq -wt $KERNEL 2>&1| tail -1)
         echo "D2_2: $D2_2 ms" >&2
+        CA57_2=$(taskset -c 0 ./bin2/easypap -k blur --no-display --load-image images/1024.png -i 100 -v seq -wt $KERNEL 2>&1| tail -1)
+        echo "CA57_2: $CA57_2 ms" >&2
 
         ## if the execution time is less than the previous one, we update the value in the array
         if [ $(echo "$D2_2 < ${D2[2]}" | bc) -eq 1 ]
@@ -100,17 +100,17 @@ do
             echo "update D2_2" >&2
         fi
 
-        if [ $(echo "$CA52_2 < ${CA52[2]}" | bc) -eq 1 ]
+        if [ $(echo "$CA57_2 < ${CA57[2]}" | bc) -eq 1 ]
         then
-            CA52[2]=$CA52_2
-            echo "update CA52_2" >&2
+            CA57[2]=$CA57_2
+            echo "update CA57_2" >&2
         fi
 
         ## O3
-        D2_3=$(taskset -c 1 ./bin3/easypap -k blur --load-image images/1024.png -i 100 -v seq -wt $KERNEL 2>&1| tail -1)
+        D2_3=$(taskset -c 1 ./bin3/easypap -k blur --no-display --load-image images/1024.png -i 100 -v seq -wt $KERNEL 2>&1| tail -1)
         echo "D2_3: $D2_3 ms" >&2
-        CA52_3=$(taskset -c 0 ./bin3/easypap -k blur --load-image images/1024.png -i 100 -v seq -wt $KERNEL 2>&1| tail -1)
-        echo "CA52_3: $CA52_3 ms" >&2
+        CA57_3=$(taskset -c 0 ./bin3/easypap -k blur --no-display --load-image images/1024.png -i 100 -v seq -wt $KERNEL 2>&1| tail -1)
+        echo "CA57_3: $CA57_3 ms" >&2
 
         # if the execution time is less than the previous one, we update the value in the array
         if [ $(echo "$D2_3 < ${D2[3]}" | bc) -eq 1 ]
@@ -119,10 +119,10 @@ do
             echo "update D2_3">&2
         fi
 
-        if [ $(echo "$CA52_3 < ${CA52[3]}" | bc) -eq 1 ]
+        if [ $(echo "$CA57_3 < ${CA57[3]}" | bc) -eq 1 ]
         then
-            CA52[3]=$CA52_3
-            echo "update CA52_3" >&2
+            CA57[3]=$CA57_3
+            echo "update CA57_3" >&2
         fi
     done
 
@@ -133,14 +133,14 @@ do
     echo 
     echo "#############  O0  ###############"
     echo "D2: ${D2[0]} ms"
-    echo "CA52: ${CA52[0]} ms"
+    echo "CA57: ${CA57[0]} ms"
     echo "#############  O1  ###############"
     echo "D2: ${D2[1]} ms"
-    echo "CA52: ${CA52[1]} ms"
+    echo "CA57: ${CA57[1]} ms"
     echo "#############  O2  ###############"
     echo "D2: ${D2[2]} ms"
-    echo "CA52: ${CA52[2]} ms"
+    echo "CA57: ${CA57[2]} ms"
     echo "#############  O3  ###############"
     echo "D2: ${D2[3]} ms"
-    echo "CA52: ${CA52[3]} ms"
+    echo "CA57: ${CA57[3]} ms"
 done
--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 taskset -c 1 ./bin/easypap -k blur --load-image images/1024.png -i 5 -v seq -wt default_nb -n -sh 1>&2 2>/dev/null 
+taskset -c 1 ./bin/easypap -k blur_v2 --load-image images/1024.png -i 5 -v seq -wt default_nb -n -sh 1>&2 2>/dev/null 
 
-for KERNEL in "optim1" "optim2" "optim3" "optim4"
+for V in "" "_v2"
 do
-    echo "test $KERNEL"
-    taskset -c 1 ./bin/easypap -k blur --load-image images/1024.png -i 5 -v seq -wt $KERNEL -n -sh 1>&2 2>/dev/null
-    diff data/hash/blur-seq-$KERNEL-dim-1024-iter-5-arg-none.sha256 data/hash/blur-seq-default_nb-dim-1024-iter-5-arg-none.sha256 && echo "OK" || echo "TEST $KERNEL FAILED"
+    for KERNEL in "optim1" "optim2" "optim3" "optim4"
+    do
+        echo "test $KERNEL$V"
+        taskset -c 1 ./bin/easypap -k blur$V --load-image images/1024.png -i 5 -v seq -wt $KERNEL -n -sh 1>&2 2>/dev/null
+        diff data/hash/blur$V-seq-$KERNEL-dim-1024-iter-5-arg-none.sha256 data/hash/blur$V-seq-default_nb-dim-1024-iter-5-arg-none.sha256 && echo "OK" || echo "TEST $KERNEL FAILED"
+    done
 done
