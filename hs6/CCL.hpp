9bd6c99e170565e0ff4c0ca069b5cfc7d7e71ef5
MengqianXu <116844816+MengqianXu@users.noreply.github.com>
2024-10-22 00:57:50 +0200

    Create CCL.hpp

---
+++ hs6/CCL.hpp
@@ -0,0 +1,24 @@
+#ifndef CCL_HPP
+#define CCL_HPP
+
+#include "spu/module/stateful.hpp"   
+#include "motion/CCL.h"              
+
+class CCL : public spu::module::Stateful {
+public:
+    /**
+     * Constructor to initialize the CCL module
+     * @param ccl_data Pointer to the CCL_data_t structure used for CCL operations
+     * @param i0 Starting y-coordinate of the image
+     * @param i1 Ending y-coordinate of the image
+     * @param j0 Starting x-coordinate of the image
+     * @param j1 Ending x-coordinate of the image
+     */
+    CCL(CCL_data_t* ccl_data, int i0, int i1, int j0, int j1);
+
+private:
+    CCL_data_t* ccl_data;  
+    int i0, i1, j0, j1;  
+};
+
+#endif 
