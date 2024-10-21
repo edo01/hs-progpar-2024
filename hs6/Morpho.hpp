4da30b6872f82ccc1c0f5d0409d793b4f4795122
MengqianXu <116844816+MengqianXu@users.noreply.github.com>
2024-10-22 00:56:30 +0200

    Create Morpho.hpp

---
+++ hs6/Morpho.hpp
@@ -0,0 +1,23 @@
+#ifndef MORPHO_HPP
+#define MORPHO_HPP
+
+#include "spu/module/stateful.hpp"   
+#include "motion/morpho.h"           
+
+/**
+ * Constructor to initialize the Morpho module
+ * @param morpho_data Pointer to the morpho_data_t structure used for morphological operations
+ * @param i0 Starting y-coordinate of the image
+ * @param i1 Ending y-coordinate of the image
+ * @param j0 Starting x-coordinate of the image
+ * @param j1 Ending x-coordinate of the image
+ */
+class Morpho : public spu::module::Stateful {
+public:
+    Morpho(morpho_data_t* morpho_data, int i0, int i1, int j0, int j1);
+private:
+    morpho_data_t* morpho_data;  
+    int i0, i1, j0, j1;  
+};
+
+#endif 
