8c3aba0b6eaadc7af573998b7fe4f597969a2201
MengqianXu <116844816+MengqianXu@users.noreply.github.com>
2024-11-03 13:03:16 +0100

    Update FirstTry.cu

--- MonteCarlo/FirstTry.cu
+++ MonteCarlo/FirstTry.cu
@@ -28,21 +28,52 @@ void testCUDA(cudaError_t error, const char* file, int line) {
 // macros __FILE__ and __LINE__
 #define testCUDA(error) (testCUDA(error, __FILE__ , __LINE__))
 
+/*One-Dimensional Normal Law. Cumulative distribution function. */
+double NP(double x) {
+	const double p = 0.2316419;
+	const double b1 = 0.319381530;
+	const double b2 = -0.356563782;
+	const double b3 = 1.781477937;
+	const double b4 = -1.821255978;
+	const double b5 = 1.330274429;
+	const double one_over_twopi = 0.39894228;
+	double t;
 
-__global__ void hestonMonteCarlo(float *d_results, int steps, float dt, float kappa, float theta, float sigma, float rho) {
+	if (x >= 0.0) {
+		t = 1.0 / (1.0 + p * x);
+		return (1.0 - one_over_twopi * exp(-x * x / 2.0) * t * (t * (t *
+			(t * (t * b5 + b4) + b3) + b2) + b1));
+	}
+	else {/* x < 0 */
+		t = 1.0 / (1.0 - p * x);
+		return (one_over_twopi * exp(-x * x / 2.0) * t * (t * (t * (t *
+			(t * b5 + b4) + b3) + b2) + b1));
+	}
+}
+
+// Set the state for each thread
+__global__ void init_curand_state(curandState_t* state)
+{
+	int idx = blockDim.x * blockIdx.x + threadIdx.x;
+	/* Each thread gets same seed, a different sequence
+	   number, no offset */
+	curand_init(0, idx, 0, &state[idx]);
+}
+
+__global__ void hestonMonteCarlo(float *d_results, int steps, float dt, float kappa, float theta, float sigma, float rho, curandState_t* state) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
 
     //Initialize the random number generator
-    curandState state;
-    curand_init(1234, tid, 0, &state);
+    int idx = blockDim.x * blockIdx.x + threadIdx.x;
+	curandState localState = state[idx];
 
     float St = S0;
     float vt = v0;
 
     //Simulation time step
     for (int i = 0; i < steps; ++i) {
-        float G1 = curand_normal2(&state);
-        float G2 = curand_normal2(&state);
+        float G1 = curand_normal(&localState);
+        float G2 = curand_normal(&localState);
 
         // Calculate the delta of asset price and volatility
         float dSt = r * St * dt + sqrtf(vt) * St * sqrtf(dt) * (rho * G1 + sqrtf(1 - rho * rho) * G2);
@@ -57,12 +88,19 @@ __global__ void hestonMonteCarlo(float *d_results, int steps, float dt, float ka
 
 
 int main() {
+    
+    int NTPB = 256;
+    int NB = (simulations + NTPB - 1) / NTPB;
+    
     //Allocate memory on the device to store the results
     float *d_results;
     cudaMalloc((void **)&d_results, simulations * sizeof(float));
 
-    int threadsPerBlock = 256;
-    int blocks = (simulations + threadsPerBlock - 1) / threadsPerBlock;
+    curandState_t* state;
+	// cudaMalloc the array state
+	cudaMalloc(&state, simulations * sizeof(curandState_t)); // is the total number of state
+
+    init_curand_state<<<NB, NTPB>>>(state);
 
 	float Tim;
 	cudaEvent_t start, stop;			// GPU timer instructions
@@ -70,7 +108,7 @@ int main() {
 	cudaEventCreate(&stop);				// GPU timer instructions
 	cudaEventRecord(start, 0);			// GPU timer instructions
 
-    hestonMonteCarlo<<<blocks, threadsPerBlock>>>(d_results, steps, dt, kappa, theta, sigma, rho);
+    hestonMonteCarlo<<<NB, NTPB>>>(d_results, steps, dt, kappa, theta, sigma, rho, state);
 
 	cudaEventRecord(stop, 0);			// GPU timer instructions
 	cudaEventSynchronize(stop);			// GPU timer instructions
@@ -91,6 +129,12 @@ int main() {
 
     std::cout << "Option Price: " << option_price << std::endl;
 
+    printf("The estimated price is equal to %f\n", option_price);
+
+	printf("The true price %f\n", S0 * NP((r + 0.5 * sigma * sigma)/sigma) -
+									K * expf(-r) * NP((r - 0.5 * sigma * sigma) / sigma));
+
+	printf("Execution time %f ms\n", Tim);
     free(h_results);
     cudaFree(d_results);
 
