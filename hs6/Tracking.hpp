9d3c5c3ae104d942da2ab6c890a9938f112106e1
edo01 <edocarra012@gmail.com>
2024-11-03 15:49:16 +0100

    add tasks 6

--- hs6/Tracking.hpp
+++ hs6/Tracking.hpp
@@ -1,7 +1,3 @@
-//tracking_perform(tracking_data, RoIs1, n_RoIs1, cur_fra, p_trk_ext_d, p_trk_obj_min,
-//                         p_trk_roi_path != NULL || visu, p_trk_ext_o, p_knn_s);
-//const RoI_t *RoIs, size_t n_RoIs, size_t frame, size_t r_extrapol, size_t fra_obj_min, 
-// uint8_t save_RoIs_id, uint8_t extrapol_order_max, float min_extrapol_ratio_S)
 #ifndef TRACKING_HPP
 #define TRACKING_HPP
 
@@ -11,14 +7,13 @@
 
 class Tracking : public spu::module::Stateful {
 public:
-    Tracking(tracking_data_t* tracking_data, int p_cca_roi_max2, size_t frame, size_t r_extrapol, 
+    Tracking(tracking_data_t* tracking_data, int p_cca_roi_max2, size_t r_extrapol, 
             size_t fra_obj_min, uint8_t save_RoIs_id, uint8_t extrapol_order_max, 
             float min_extrapol_ratio_S); 
 
 private:
     tracking_data_t* tracking_data;
     size_t p_cca_roi_max2;
-    size_t frame;
     size_t r_extrapol;
     size_t fra_obj_min;
     uint8_t save_RoIs_id;
