592e4f6abfd9b88717bfedd8cfa4e48382e936fa
MengqianXu <xumengqian201808@163.com>
2024-11-07 13:37:12 +0100

    Update CCL.hpp

--- hs7/deep_copy/CCL.hpp
+++ hs7/deep_copy/CCL.hpp
@@ -6,29 +6,28 @@
 #include "motion/CCL.h"              
 
 class CCL : public spu::module::Stateful {
+    private:
+        CCL_data_t* ccl_data;   
+        int def_p_cca_roi_max;
     public:
         /**
          * Constructor to initialize the CCL module
          * @param ccl_data Pointer to the CCL_data_t structure used for CCL operations
          */
-        CCL(CCL_data_t* ccl_data, int def_p_cca_roi_max);
+        //CCL(CCL_data_t* ccl_data, int def_p_cca_roi_max);
 
-    private:
-        CCL_data_t* ccl_data;   
-        int def_p_cca_roi_max;
-        
-    CCL* CCL::clone() const {
-        auto c = new CCL(*this);  
-        c->deep_copy(*this);      
-        return c;
-    }
+        CCL* CCL::clone() const {
+            auto c = new CCL(*this);  
+            c->deep_copy(*this);      
+            return c;
+        }
 
-    void CCL::deep_copy(const CCL& c) {
-        Stateful::deep_copy(c);  
-        this->ccl_data = ccl_alloc_data(c.ccl_data->i0, c.ccl_data->i1,
-                                        c.ccl_data->j0, c.ccl_data->j1);
-        ccl_init_data(this->ccl_data);
-    }
+        void CCL::deep_copy(const CCL& c) {
+            Stateful::deep_copy(c);  
+            this->ccl_data = ccl_alloc_data(c.ccl_data->i0, c.ccl_data->i1,
+                                            c.ccl_data->j0, c.ccl_data->j1);
+            ccl_init_data(this->ccl_data);
+        }
 };
 
 #endif 
