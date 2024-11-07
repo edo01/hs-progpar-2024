a04a893e5e7d8e9909eea7a7cd9051bba7321e2f
MengqianXu <xumengqian201808@163.com>
2024-11-07 13:37:01 +0100

    Update Morpho.hpp

--- hs7/deep_copy/Morpho.hpp
+++ hs7/deep_copy/Morpho.hpp
@@ -14,27 +14,27 @@
  * @param j1 Ending x-coordinate of the image
  */
 class Morpho : public spu::module::Stateful {
-    public:
-        Morpho(morpho_data_t* morpho_data, int i0, int i1, int j0, int j1);
     private:
         morpho_data_t* morpho_data;  
         int i0, i1, j0, j1;  
+    public:
+        //Morpho(morpho_data_t* morpho_data, int i0, int i1, int j0, int j1);
         
-    Morpho* Morpho::clone() const {
-        auto m = new Morpho(*this);
-        m->deep_copy(*this); // we override this method just after
-        return m;
-    }
-    // in the deep_copy method, 'this' is the newly allocated object while 'm' is the former object
-    void Morpho::deep_copy(const Morpho& m) {
-        // call the 'deep_copy' method of the Module class
-        Stateful::deep_copy(m);
-        // allocate new morpho inner data
-        this->morpho_data = morpho_alloc_data(m.morpho_data->i0, m.morpho_data->i1,
-                                            m.morpho_data->j0, m.morpho_data->j1);
-        // initialize the previously allocated data
-        morpho_init_data(this->morpho_data);
-    }
+        Morpho* Morpho::clone() const {
+            auto m = new Morpho(*this);
+            m->deep_copy(*this); // we override this method just after
+            return m;
+        }
+        // in the deep_copy method, 'this' is the newly allocated object while 'm' is the former object
+        void Morpho::deep_copy(const Morpho& m) {
+            // call the 'deep_copy' method of the Module class
+            Stateful::deep_copy(m);
+            // allocate new morpho inner data
+            this->morpho_data = morpho_alloc_data(m.morpho_data->i0, m.morpho_data->i1,
+                                                m.morpho_data->j0, m.morpho_data->j1);
+            // initialize the previously allocated data
+            morpho_init_data(this->morpho_data);
+        }
 };
 
 #endif 
