af5879b0efa1e44869f70bb67a46ed22273506c4
edo01 <edocarra012@gmail.com>
2024-11-13 23:24:38 +0100

    Task vectorization

--- hs7/Sigma_delta_compute.cpp
+++ hs7/Sigma_delta_compute.cpp
@@ -1,12 +1,7 @@
-#include <omp.h>
-#include <stdint.h>
-#include <mipp.h>
-#include <cmath>  
-
-
 #include <math.h>
 #include <stdlib.h>
 #include <nrc2.h>
+#include <mipp.h>
 
 #include "motion/macros.h"
 #include "motion/sigma_delta/sigma_delta_compute.h"
@@ -48,28 +43,48 @@ void sigma_delta_compute(sigma_delta_data_t *sd_data, const uint8_t** img_in, ui
     
 #pragma omp parallel
 {
-    
-    #pragma omp for collapse(2)
+
+    // best configuration should be static,3
+    #pragma omp for schedule(runtime)
     for (int i = i0; i <= i1; i++) {
-        for (int j = j0; j <= j1; j++) {
-            uint8_t new_m = sd_data->M[i][j];
-            if (sd_data->M[i][j] < img_in[i][j])
-                new_m += 1;
-            else if (sd_data->M[i][j] > img_in[i][j])
-                new_m -= 1;
-            sd_data->M[i][j] = new_m;
+        for (int j = j0; j <= j1; j+=mipp::N<uint8_t>()) {
+            mipp::Reg<uint8_t> M_r(&sd_data->M[i][j]); // uint8_t new_m = sd_data->M[i][j]; 
+            mipp::Reg<uint8_t> img_in_r(&img_in[i][j]); // img_in[i][j]
+
+            mipp::Msk<mipp::N<uint8_t>()> mask_lt_r = M_r < img_in_r;
+            mipp::Msk<mipp::N<uint8_t>()> mask_gt_r = M_r > img_in_r;
+
+            M_r = mipp::blend(M_r + 1, M_r, mask_lt_r);
+            M_r = mipp::blend(M_r - 1, M_r, mask_gt_r);
+            M_r.store(&sd_data->M[i][j]);
         }
     }
 
-    #pragma omp for collapse(2)
+    #pragma omp for schedule(runtime)
     for (int i = i0; i <= i1; i++) {
-        for (int j = j0; j <= j1; j++) {
-            sd_data->O[i][j] = abs(sd_data->M[i][j] - img_in[i][j]);
+        for (int j = j0; j <= j1; j+=mipp::N<int8_t>()) {
+            mipp::Reg<uint8_t> M_r(&sd_data->M[i][j]);
+            mipp::Reg<uint8_t> img_in_r(&img_in[i][j]);
+
+            mipp::Msk<mipp::N<uint8_t>()> mask_lt_r = M_r > img_in_r;
+            mipp::Reg<uint8_t> res = mipp::blend(M_r - img_in_r, img_in_r - M_r, mask_lt_r);
+            res.store(&sd_data->O[i][j]);
         }
     }
 
-    #pragma omp for collapse(2)
+    #pragma omp for schedule(runtime)
     for (int i = i0; i <= i1; i++) {
+       /*  for (int j = j0; j <= j1; j+=mipp::N<int8_t>()) {
+            mipp::Reg<uint8_t> V_r(&sd_data->V[i][j]); //uint8_t new_v = sd_data->V[i][j];
+            mipp::Reg<uint8_t> O_r(&sd_data->O[i][j]);
+            
+            mipp::Msk<mipp::N<uint8_t>()> mask_lt_r = V_r < O_r * N;
+            mipp::Msk<mipp::N<uint8_t>()> mask_gt_r = V_r > O_r * N;
+            V_r = mipp::blend(V_r + 1, V_r, mask_lt_r);
+            V_r = mipp::blend(V_r - 1, V_r, mask_gt_r);
+            V_r = mipp::max(mipp::min(V_r, mipp::Reg<uint8_t>(sd_data->vmax) ), mipp::Reg<uint8_t>(sd_data->vmin));
+            V_r.store(&sd_data->V[i][j]); */
+
         for (int j = j0; j <= j1; j++) {
             uint8_t new_v = sd_data->V[i][j];
             if (sd_data->V[i][j] < N * sd_data->O[i][j])
@@ -80,77 +95,18 @@ void sigma_delta_compute(sigma_delta_data_t *sd_data, const uint8_t** img_in, ui
         }
     }
 
-    #pragma omp for collapse(2)
+    #pragma omp for schedule(runtime)
     for (int i = i0; i <= i1; i++) {
-        for (int j = j0; j <= j1; j++) {
-            img_out[i][j] = sd_data->O[i][j] < sd_data->V[i][j] ? 0 : 255;
+        for(int j = j0; j <= j1; j+=mipp::N<uint8_t>()){
+            mipp::Reg<uint8_t> O_r(&sd_data->O[i][j]);
+            mipp::Reg<uint8_t> V_r(&sd_data->V[i][j]);
+            
+            mipp::Msk<mipp::N<uint8_t>()> mask_gt = O_r < V_r;
+            mipp::Reg<uint8_t> res = mipp::blend( mipp::Reg<uint8_t>((uint8_t)0), mipp::Reg<uint8_t>((uint8_t)255), mask_gt);
+            res.store(&img_out[i][j]);
         }
     }
 
 }
 
-}
-
-//version mipp
-void sigma_delta_compute_mipp(sigma_delta_data_t *sd_data, const uint8_t** img_in, uint8_t** img_out, const int i0,
-                         const int i1, const int j0, const int j1, const uint8_t N) {
-    const int width = j1 - j0 + 1;
-    const int simd_width = mipp::N<uint8_t>(); //Get the SIMD vector width supported by the current platform
-
-    for (int i = i0; i <= i1; i++) {
-        for (int j = j0; j <= j1; j += simd_width) {
-            mipp::Reg<uint8_t> M_r(&sd_data->M[i][j]);
-            mipp::Reg<uint8_t> img_in_r(&img_in[i][j]);
-            mipp::Reg<uint8_t> inc_r = mipp::Reg<uint8_t>(1);
-
-            mipp::Reg<uint8_t> mask_lt_r = M_r < img_in_r;
-            mipp::Reg<uint8_t> mask_gt_r = M_r > img_in_r;
-
-            M_r = mipp::blend(mask_lt_r, M_r + inc_r, M_r);
-            M_r = mipp::blend(mask_gt_r, M_r - inc_r, M_r);
-
-            M_r.store(&sd_data->M[i][j]);
-        }
-    }
-
-
-    for (int i = i0; i <= i1; i++) {
-        for (int j = j0; j <= j1; j += simd_width) {
-            mipp::Reg<uint8_t> M_r(&sd_data->M[i][j]);
-            mipp::Reg<uint8_t> img_in_r(&img_in[i][j]);
-
-            mipp::Reg<uint8_t> O_r = mipp::abs(M_r - img_in_r);
-            O_r.store(&sd_data->O[i][j]);
-        }
-    }
-
-    for (int i = i0; i <= i1; i++) {
-        for (int j = j0; j <= j1; j += simd_width) {
-            mipp::Reg<uint8_t> V_r(&sd_data->V[i][j]);
-            mipp::Reg<uint8_t> O_r(&sd_data->O[i][j]);
-            mipp::Reg<uint8_t> inc_r = mipp::Reg<uint8_t>(1);
-            mipp::Reg<uint8_t> N_r = mipp::Reg<uint8_t>(N);
-
-            mipp::Reg<uint8_t> target_r = N_r * O_r;
-
-            mipp::Reg<uint8_t> mask_lt_r = V_r < target_r;
-            mipp::Reg<uint8_t> mask_gt_r = V_r > target_r;
-
-            V_r = mipp::blend(mask_lt_r, V_r + inc_r, V_r);
-            V_r = mipp::blend(mask_gt_r, V_r - inc_r, V_r);
-
-            V_r = mipp::min(mipp::max(V_r, mipp::Reg<uint8_t>(sd_data->vmin)), mipp::Reg<uint8_t>(sd_data->vmax));
-            V_r.store(&sd_data->V[i][j]);
-        }
-    }
-
-    for (int i = i0; i <= i1; i++) {
-        for (int j = j0; j <= j1; j += simd_width) {
-            mipp::Reg<uint8_t> O_r(&sd_data->O[i][j]);
-            mipp::Reg<uint8_t> V_r(&sd_data->V[i][j]);
-
-            mipp::Reg<uint8_t> out_r = mipp::blend(O_r < V_r, mipp::Reg<uint8_t>(0), mipp::Reg<uint8_t>(255));
-            out_r.store(&img_out[i][j]);
-        }
-    }
 }
